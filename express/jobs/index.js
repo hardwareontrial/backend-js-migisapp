38687ec5254f0203b962fc2db448ed07747fa464
hardwareontrial <hardwareontrial@gmail.com>
2023-06-05 09:37:32 +0700

    set module for job

--- express/jobs/index.js
+++ express/jobs/index.js
@@ -4,8 +4,8 @@ const cron = require('node-cron');
 const AttendanceUserController = require('../../sequelize/controllers/AttendanceUserController');
 
 exports.AttendanceUser = () => {
-  const attendanceSync = cron.schedule('* * * * *', () => {
-    // AttendanceUserController.testing()
+  const attendanceSync = cron.schedule('58 0,7,11,16 * * *', () => {
+    // AttendanceUserController.attSync()
   });
   attendanceSync.start();
 }
